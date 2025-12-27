08a25fc6539cef217d3543cf5ae9961b65eba6d1
oc3t <ocetamulyaaa@gmail.com>
2025-12-27 15:36:56 +0700

    updt5

--- dist-static/env.js
+++ dist-static/env.js
@@ -1,5 +1,5 @@
 // Isi dengan kredensial Supabase Anda (anon key aman untuk client-side).
 window.__APP_CONFIG = {
   supabaseUrl: "https://hncgwshkcxryxilwsjmf.supabase.co",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhuY2d3c2hrY3hyeXhpbHdzam1mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzOTE5NTYsImV4cCI6MjA3ODk2Nzk1Nn0.JU2_jCWfpSzInY5BcPHw15EFQ8eXC1QCqUXvUgVlCKs"
+  supabaseAnonKey: "sb_publishable_paHs-nHjBnj6YaK6g_Z0fA_g5hhnZgl"
 };
