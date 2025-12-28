9b55178801595bf367099f59edb4173f2cb29751
oc3t <ocetamulyaaa@gmail.com>
2025-12-28 19:50:39 +0700

    updt20

--- dist-static/env.js
+++ dist-static/env.js
@@ -1,5 +1,7 @@
 // Isi dengan kredensial Supabase Anda (anon key aman untuk client-side).
 window.__APP_CONFIG = {
   supabaseUrl: "https://hncgwshkcxryxilwsjmf.supabase.co",
-  supabaseAnonKey: "sb_publishable_paHs-nHjBnj6YaK6g_Z0fA_g5hhnZgl"
+  supabaseAnonKey: "sb_publishable_paHs-nHjBnj6YaK6g_Z0fA_g5hhnZgl",
+  // Ubah jika bucket storage Anda bukan "jerseys"
+  storageBucket: "jerseys"
 };
